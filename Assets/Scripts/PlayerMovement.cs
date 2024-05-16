c9b7642d850e036523269a7c290caa2a71cc8cc5
Plaskning <102387003+Plaskning@users.noreply.github.com>
2024-05-16 13:27:06 +0200

    Project added

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //Inputs
+    InputAction moveAction;
+    InputAction jumpAction;
+
+
+    private float movementSpeed;
+    private float coyoteTime;
+
+    //Jump
+    private float jumpStrength;
+    private float jumpBufferTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
